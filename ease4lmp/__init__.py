522b958e57d289e7f43615e3ab656af5ee1efe35
Takayuki Kobayashi <iris.takayuki@gmail.com>
2019-02-20 15:42:26 +0900

    add __version__ attribute

--- ease4lmp/__init__.py
+++ ease4lmp/__init__.py
@@ -1,5 +1,7 @@
 """Extension of Atomic Simulation Environment (ASE) for LAMMPS"""
 
+from ._version import __version__
+
 from .bonded_atoms import (
   BondedAtoms, create_atoms_from_json,
   create_atoms_from_data, create_atoms_from_molecule)
@@ -13,6 +15,7 @@ from .lammps_reader import (
 from .lammps_units import lmp_units
 
 __all__ = [
+  "__version__",
   "BondedAtoms",
   "create_atoms_from_json",
   "create_atoms_from_data",
