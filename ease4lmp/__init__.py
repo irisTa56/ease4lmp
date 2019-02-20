0dc5540e9530f945e776c0b6f94b9087e9133eac
Takayuki Kobayashi <iris.takayuki@gmail.com>
2019-02-20 15:22:06 +0900

    make lmp_units in lammps_units.py available

--- ease4lmp/__init__.py
+++ ease4lmp/__init__.py
@@ -3,10 +3,14 @@
 from .bonded_atoms import (
   BondedAtoms, create_atoms_from_json,
   create_atoms_from_data, create_atoms_from_molecule)
+
 from .lammps_writer import LammpsWriter
+
 from .lammps_reader import (
   read_bonds, read_angles, read_dihedrals, read_impropers,
   read_atoms_from_data, read_atoms_from_molecule)
+
+from .lammps_units import lmp_units
 
 __all__ = [
   "BondedAtoms",
@@ -20,4 +24,5 @@ __all__ = [
   "read_impropers",
   "read_atoms_from_data",
   "read_atoms_from_molecule",
+  "lmp_units",
 ]
